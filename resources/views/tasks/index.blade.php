5787c47814139a21d78a3b858713464ca14594e1
Luli Zoltán Győző <luli.zoltan@gmail.com>
2020-12-09 22:39:11 +0100

    - Kisebb módosítások

--- resources/views/tasks/index.blade.php
+++ resources/views/tasks/index.blade.php
@@ -3,18 +3,21 @@
 @section('content')
     
     <h1>Feladatok</h1>
-    {{ Form::checkbox('isFinished',null,['onchange'=>'App\Http\Controllers\TaskController@index']) }}
-
+    {!! Form::open([]) !!}
+        {{ Form::checkbox('isFinished',null,['onchange'=>'App\Http\Controllers\TaskController@index']) }}
+    {!! Form::close() !!}
     @if(count($tasks) > 0)
         @foreach ($tasks as $task)
             <div class="well">
             <h3><a href="/tasks/{{$task->id}}">{{$task->task_title}}</a></h3>
                 <small>{{$task->task_content}}</small>
-                @if($task->isFinished)
-                @else
+                @if($task->isFinished = 0) <div>Állapot: Kész</div>
+                @else <div>Állapot: Folyamatban</div>
                 @endif
-                <div>Módosítva:{{$task->create_at}}</div>
+                <hr> 
+                <div>Módosítva:{{$task->created_at}}</div>
             </div>
+            <hr>
         @endforeach
         
     @else
